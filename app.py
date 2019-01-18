40b0a0dffdf2724b1563eaef8e2106a456ba5743
lhuddlesto <lhuddlesto@gannett.com>
2019-01-17 19:37:08 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+# Python Data Types
+
+students_count = 1000 # Integer, underscores are the common spacing convention in Python
+rating = 4.99 # Float
+is_published = False # Booleans are always uppercased
+course_name = "Python" # Can use single, double, or triple quotes
+
+# Initializing in Python
+## Multiple variables can be initialized on the same line
+
+x = 1
+y = 2
+x, y = 1, 2
+
+# You can also set multiple variables to the same value
+
+x = y = 1
